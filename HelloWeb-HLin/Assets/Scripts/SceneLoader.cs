f4d27ea3411254f2f0922712d333e4d988bd4f37
chiini <chiini@hotmail.de>
2019-02-27 17:32:20 +0100

    added SceneSwitcher

---
+++ HelloWeb-HLin/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+	public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadPreviousScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
+
